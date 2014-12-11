8e7c7d1f3136d3de38ff54ede861aa4d1145ab2a
Mike <mike@silverstripe.com>
2014-12-12 10:10:04 +1300

    FIX: PHP Strict Standards warning 'Declaration of MyCrawler::handleDocumentInfo()'

--- example.php
+++ example.php
@@ -6,10 +6,10 @@ set_time_limit(10000);
 // Inculde the phpcrawl-mainclass
 include("libs/PHPCrawler.class.php");
 
-// Extend the class and override the handleDocumentInfo()-method 
-class MyCrawler extends PHPCrawler 
+// Extend the class and override the handleDocumentInfo()-method
+class MyCrawler extends PHPCrawler
 {
-  function handleDocumentInfo($DocInfo) 
+  function handleDocumentInfo(PHPCrawlerDocumentInfo $DocInfo)
   {
     // Just detect linebreak for output ("\n" in CLI-mode, otherwise "<br>").
     if (PHP_SAPI == "cli") $lb = "\n";
@@ -17,23 +17,23 @@ class MyCrawler extends PHPCrawler
 
     // Print the URL and the HTTP-status-Code
     echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")".$lb;
-    
+
     // Print the refering URL
     echo "Referer-page: ".$DocInfo->referer_url.$lb;
-    
+
     // Print if the content of the document was be recieved or not
     if ($DocInfo->received == true)
       echo "Content received: ".$DocInfo->bytes_received." bytes".$lb;
     else
-      echo "Content not received".$lb; 
-    
+      echo "Content not received".$lb;
+
     // Now you should do something with the content of the actual
-    // received page or file ($DocInfo->source), we skip it in this example 
-    
+    // received page or file ($DocInfo->source), we skip it in this example
+
     echo $lb;
-    
+
     flush();
-  } 
+  }
 }
 
 // Now, create a instance of your class, define the behaviour
@@ -67,10 +67,10 @@ $report = $crawler->getProcessReport();
 
 if (PHP_SAPI == "cli") $lb = "\n";
 else $lb = "<br />";
-    
+
 echo "Summary:".$lb;
 echo "Links followed: ".$report->links_followed.$lb;
 echo "Documents received: ".$report->files_received.$lb;
 echo "Bytes received: ".$report->bytes_received." bytes".$lb;
-echo "Process runtime: ".$report->process_runtime." sec".$lb; 
+echo "Process runtime: ".$report->process_runtime." sec".$lb;
 ?>
